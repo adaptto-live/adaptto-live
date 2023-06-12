28fed34fedb225955ca51c2c77d9c2b3eabd069e
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-06-12 14:13:18 +0200

    display client version

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -14,12 +14,17 @@
       <th>Admin</th>
       <td><code>{{authenticationStore.admin}}</code></td>
     </tr>
+    <tr>
+      <th>Client Version</th>
+      <td><code>{{version}}</code></td>
+    </tr>
   </table>
   
 </template>
 
 <script setup lang="ts">
 import { useAuthenticationStore } from '@/stores/authentication'
+import { version} from '@/../package.json'
 
 const authenticationStore = useAuthenticationStore()
 </script>
