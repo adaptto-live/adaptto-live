fc9d7952b568e3c94b9e246617fcf5b0c1290194
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-06-13 18:22:10 +0200

    ask user to follow to talk if current talk changes

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,59 @@
         {{talkStore.loadingError}}
       </div>
       <RouterView :key="$route.fullPath"/>
+
+      <div class="modal" tabindex="-1" ref="currentTalkChangeModal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5">Current Talk Change</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <p>The current talk has changed.</p>
+              <p>Go to talk <strong>{{currentTalk?.title}}</strong>?</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="goToCurrentTalk">OK</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue'
-import { RouterView, useRoute } from 'vue-router'
+import { onMounted, onUnmounted, ref } from 'vue'
+import { RouterView, useRoute, useRouter } from 'vue-router'
 import AppHeader from './components/structure/AppHeader.vue'
 import { useAuthenticationStore } from './stores/authentication'
 import socket from './util/socket'
 import { useRatingStore } from './stores/rating'
 import { useCurrentTalkStore } from './stores/currentTalk'
-import { useTalksStore } from './stores/talks'
+import { useTalksStore, type Talk } from './stores/talks'
+import { Modal } from 'bootstrap'
+import TalkManager from './services/TalkManager'
 
-const talkStore = useTalksStore()
 const authenticationStore = useAuthenticationStore()
+const talkStore = useTalksStore()
 const currentTalkStore = useCurrentTalkStore()
 const ratingStore = useRatingStore()
 const route = useRoute()
+const router = useRouter()
+const talkManager = new TalkManager()
+
+const currentTalkChangeModal = ref(undefined as HTMLElement|undefined)
+const currentTalkId = ref(undefined as string|undefined)
+const currentTalk = ref(undefined as Talk|undefined)
+
+function goToCurrentTalk() {
+  if (currentTalkId.value) {
+    router.push(`/talk/${currentTalkId.value}`)
+  }
+}
 
 onMounted(() => {
   if (authenticationStore.isAuthenticated) {
@@ -35,7 +69,15 @@ onMounted(() => {
   }
 
   socket.on('currentTalk', talkId => {
+    const talkChanged = talkId != currentTalkStore.talkId && currentTalkStore.talkId != undefined
     currentTalkStore.set(talkId)
+    if (talkChanged && currentTalkChangeModal.value) {
+      currentTalkId.value = talkId
+      currentTalk.value = talkManager.getTalk(talkId)
+      if (!currentTalkChangeModal.value.classList.contains('show')) {
+        new Modal(currentTalkChangeModal.value).show()
+      }
+    }
   })
   socket.on('talkRating', (talkId: string, rating?: number, comment?: string) => {
     if (rating) {
