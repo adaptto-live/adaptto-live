21f556444901d340a4d79d24f4fb9111fa8d886b
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-06-29 18:33:10 +0200

    no padding for apple icon

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig, minimalPreset as preset } from '@vite-pwa/assets-generator/config'
+import { Preset, defineConfig } from '@vite-pwa/assets-generator/config'
+
+const minimalPreset: Preset = {
+  transparent: {
+    sizes: [64, 192, 512],
+    favicons: [[64, 'favicon.ico']]
+  },
+  maskable: {
+    sizes: [512]
+  },
+  apple: {
+    sizes: [180],
+    padding: 0
+  }
+}
 
 export default defineConfig({
-  preset,
+  preset: minimalPreset,
   images: [
     'public/adaptto-favicon.svg'
   ]
