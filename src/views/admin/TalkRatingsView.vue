719071a7904da227913dc3d4958b200c27ba0464
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-09-29 13:49:32 +0200

    improve display of talk ratings prepare excel export

--- src/views/admin/TalkRatingsView.vue
+++ src/views/admin/TalkRatingsView.vue
@@ -12,17 +12,64 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="talkRating in talkRatings" :key="talkRating.talk.id">
-        <td>{{talkRating.talk.title}}</td>
-        <td>{{talkRating.talk.speakers}}</td>
-        <td>{{talkRating.result?.averageRating.toFixed(2)}}</td>
-        <td>{{talkRating.result?.participants}}</td>
-        <td>
-          <ul v-if="talkRating.result?.comments" class="small">
-            <li v-for="(comment,index) in talkRating.result?.comments" :key="index">{{comment}}</li>
-          </ul>
-        </td>
+      <template v-for="talkRating in talkRatings" :key="talkRating.talk.id">
+        <tr v-if="!talkRating.talk.lobby">
+          <td>{{talkRating.talk.title}}</td>
+          <td>{{talkRating.talk.speakers}}</td>
+          <td>{{formatRating(talkRating.result?.averageRating)}}</td>
+          <td>{{talkRating.result?.participants}}</td>
+          <td>
+            <template v-if="talkRating.result?.comments">
+              <button type="button" class="btn btn-secondary btn-sm" data-bs-toggle="modal" :data-bs-target="`#modal-comments-${talkRating.talk.id}`">
+                {{talkRating.result.comments.length}} Comments
+              </button>
+              <div class="modal" :id="`modal-comments-${talkRating.talk.id}`" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-lg modal-dialog-scrollable">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5">{{talkRating.talk.title}}</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <ul>
+                        <li v-for="(comment,index) in talkRating.result?.comments" :key="index">{{comment}}</li>
+                      </ul>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </template>
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+
+  <table class="excel-export">
+    <thead>
+      <tr>
+        <th>Talk</th>
+        <th>Speaker</th>
+        <th>Rating</th>
+        <th>Participants</th>
+        <th>Comments</th>
       </tr>
+    </thead>
+    <tbody>
+      <template v-for="talkRating in talkRatings" :key="talkRating.talk.id">
+        <tr v-if="!talkRating.talk.lobby">
+          <td>{{talkRating.talk.title}}</td>
+          <td>{{talkRating.talk.speakers}}</td>
+          <td>{{formatRatingLocalized(talkRating.result?.averageRating)}}</td>
+          <td>{{talkRating.result?.participants}}</td>
+          <td>
+            <div v-for="(comment,index) in talkRating.result?.comments" :key="index">{{comment}}</div>
+          </td>
+        </tr>
+      </template>
     </tbody>
   </table>
 
@@ -46,10 +93,27 @@ socket.on('adminTalkRatings', (ratings: AverageTalkRating[]) => {
     talk, result: ratings.find(item => item.talkId == talk.id)
   }))
 })
+
+function formatRating(rating? : number) : string|undefined {
+  return rating?.toFixed(2)
+}
+
+function formatRatingLocalized(rating? : number) : string|undefined {
+  if (rating) {
+    return rating.toLocaleString('de-DE', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 td {
   word-break: break-word;
+}
+
+.excel-export {
+  display: none;
 }
 </style>
