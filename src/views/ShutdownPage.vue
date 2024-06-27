b9f6f27bd7618534b8568f8d492def1c4b344a12
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-06-27 10:30:53 +0200

    Shutdown Page (#24)

---
+++ src/views/ShutdownPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="shutdown">
+    <img src="@/assets/logo-on-transparent.svg" alt=""/>
+    <h1>adaptTo() Live</h1>
+    <p>is not running.</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.shutdown {
+  text-align: center;
+}
+img {
+  margin: 50px;
+  width: 150px;
+}
+</style>
