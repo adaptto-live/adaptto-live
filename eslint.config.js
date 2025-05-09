dc1af7c1e67fa7e0d26775bf4f146fde7de0bc5d
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-05-09 22:32:53 +0200

    switch to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: [
+      'tests/**/*.{j,t}s?(x)',
+      'src/**/__tests__/*'
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off'
+    },
+  }
+
+)
